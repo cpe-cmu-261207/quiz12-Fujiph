036a800baf15e093a3dd3370cf76a80594bfa5a1
Fujiph <aueboonruanekamfu@gmail.com>
2021-01-12 13:54:50 +0700

    V0

--- app_template.js
+++ app_template.js
@@ -1,1 +1,6 @@
 //your code here!
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('form').onsubmit = function(){
+        const level = document.querySelector('compute-result');
+    return 620612170 level;
+})
